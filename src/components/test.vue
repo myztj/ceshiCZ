b30c3adce9861043fbb07cebd061570d8f266c75
230371647 <499458100@qq.com>
2023-04-13 15:33:32 +0800

    fixd:测试

---
+++ src/components/test.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="">
+        我是测试
+    </div>
+</template>
+
+<script>
+export default {
+    name: '',
+    components: {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
